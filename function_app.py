5d55a5d5b8588a2ee5510de5e70ad4a723a4c311
Markus Seeli <markus@seeli.eu>
2023-11-30 13:18:43 +0100

    Added more functions

--- function_app.py
+++ function_app.py
@@ -3,6 +3,9 @@ import logging
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
+# Passing data to the function via route parameter
+# Example: http://localhost:7071/api/parameter_by_route/Markus
+
 
 @app.route(route="parameter_by_route/{name}")
 def parameter_by_route(req: func.HttpRequest) -> func.HttpResponse:
@@ -11,6 +14,40 @@ def parameter_by_route(req: func.HttpRequest) -> func.HttpResponse:
     name = req.route_params.get('name')
 
     info = f"The function 'parameter_by_route' was called with the parameter: {name}"
+
+    logging.info(info)
+
+    # a http triggered function always returns a http-response
+    return func.HttpResponse(info)
+
+
+# Passing data to the function via query parameter
+# Example http://localhost:7071/api/parameter_by_query?name=Markus
+@app.route(route="parameter_by_query")
+def parameter_by_route(req: func.HttpRequest) -> func.HttpResponse:
+
+    # Get the value of the query parameter
+    name = req.params.get('name')
+
+    info = f"The function 'parameter_by_query' was called with the parameter: {name}"
+
+    logging.info(info)
+
+    # a http triggered function always returns a http-response
+    return func.HttpResponse(info)
+
+
+# Passing data to the function via message body
+# Example POST http://localhost:7071/api/parameter_by_body
+# Body: {"name": "Markus"}
+@app.route(route="parameter_by_body", methods=["POST"])
+def parameter_by_route(req: func.HttpRequest) -> func.HttpResponse:
+
+    # Get the value of the parameter out of the message body
+    req_body = req.get_json()
+    name = req_body.get('name')
+
+    info = f"The function 'parameter_by_query' was called with the parameter: {name}"
 
     logging.info(info)
 
